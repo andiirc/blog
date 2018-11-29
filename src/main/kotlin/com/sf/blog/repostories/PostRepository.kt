f7937b08b149d5e1e6c443588cdb5902972c775b
Anderson Rodriguez <devops@superfuds.com.co>
2018-11-29 17:51:31 -0500

    rename search methods

--- src/main/kotlin/com/sf/blog/repostories/PostRepository.kt
+++ src/main/kotlin/com/sf/blog/repostories/PostRepository.kt
@@ -4,13 +4,16 @@ import com.sf.blog.documents.Post
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
 import java.util.UUID
 
 @RepositoryRestResource(path = "post")
 interface PostRepository: MongoRepository<Post, UUID> {
 
+    @RestResource(path = "by-title")
     fun findByTitle(@Param("title") title: String): Post
 
+    @RestResource(path = "by-body")
     fun findByBodyLike(@Param("body") body: String ): List<Post>
 
     //fun findByCreated(@Param("created_at") create_at: String): List<Post>
